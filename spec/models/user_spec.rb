bcbdec4ac53e1086b64c8d3f24e59ff3f9db6832
Kerrie Yee <kerrie.yee@gmail.com>
2014-08-24 17:56:21 -0700

    tmp commit adding rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+  RSpec.describe User, :type => :model do
+    # it 'creates a user' do
+    #   User.create(username: 'Karen')
+    #   expect(User.count).to eql(1)
+    # end
+  end
+
